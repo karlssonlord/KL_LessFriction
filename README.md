5985e0455d9d72d008b23507e8c40f025dc16d9b
Andreas Karlsson <andreas.karlsson@indiebytes.se>
2013-04-10 17:27:24 +0200

    Add some basic stuff

---
+++ README.md
@@ -0,0 +1,19 @@
+# Less Friction Checkout by Karlsson & Lord
+
+Module description.
+
+## Install
+
+Instructions.
+
+## Get started
+
+Instructions.
+
+## Contribute
+
+Instructions.
+
+## Authors
+
+* Andreas Karlsson
---
+++ app/code/community/KL/LessFriction/Test/Main/Config.php
@@ -0,0 +1,27 @@
+<?php
+class KL_LessFriction_Test_Config_Main extends EcomDev_PHPUnit_Test_Case_Config {
+    public function testBlockAlias() {
+        $this->assertBlockAlias('lessfriction/test', 'KL_LessFriction_Block_Test');
+    }
+
+    public function testModelAlias() {
+        $this->assertModelAlias('lessfriction/test', 'KL_LessFriction_Model_Test');
+    }
+
+    public function testHelperAlias() {
+        $this->assertHelperAlias('lessfriction/test', 'KL_LessFriction_Helper_Test');
+    }
+
+    public function testCodepool() {
+        $this->assertModuleCodePool('community');
+    }
+
+    public function testDepends() {
+        $this->assertModuleDepends('Mage_Checkout');
+    }
+
+    public function testLayoutFile() {
+        $this->assertLayoutFileDefined('frontend', 'kl/checkout.xml');
+        $this->assertLayoutFileExistsInTheme('frontend', 'kl/checkout.xml', 'default', 'base');
+    }
+}
