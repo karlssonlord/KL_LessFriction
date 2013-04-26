ebe18b7127d6a40ab3586d65ff80b709dda97fee
Andreas Karlsson <andreas.karlsson@indiebytes.se>
2013-04-26 09:20:43 +0200

    Add methods to helper

--- app/code/community/KL/LessFriction/Helper/Data.php
+++ app/code/community/KL/LessFriction/Helper/Data.php
@@ -1,4 +1,22 @@
 <?php
 class KL_LessFriction_Helper_Data extends Mage_Core_Helper_Abstract
 {
+
+	protected function _getStoreConfig($group, $field)
+	{
+		$setting = Mage::getStoreConfig(sprintf('lessfriction/%s/%s', $group, $field));
+		return $setting;
+	}
+
+	public function isActive()
+	{
+		$setting = $this->_getStoreConfig('general', 'active');
+		return $setting;
+	}
+
+	public function includeCart()
+	{
+		$setting = $this->_getStoreConfig('cart', 'include_cart');
+		return $setting;
+	}
 }
