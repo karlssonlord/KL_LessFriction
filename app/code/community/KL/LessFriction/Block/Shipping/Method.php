21666279a439dd8cc1278cca6b4c2b8eeb92ccfe
Andreas Karlsson <andreas.karlsson@indiebytes.se>
2013-05-09 11:23:28 +0200

    Add support to hide shipping method section if it's pre-selected

---
+++ app/code/community/KL/LessFriction/Block/Shipping/Method.php
@@ -0,0 +1,26 @@
+<?php
+class KL_LessFriction_Block_Shipping_Method
+    extends Mage_Checkout_Block_Onepage_Shipping_Method
+{
+    /**
+     * Is shipping method block visible?
+     *
+     * @return bool
+     */
+    public function isVisible()
+    {
+        $_shippingAddress    = $this->getQuote()->getShippingAddress();
+        $_shippingRateGroups = $_shippingAddress->getGroupedAllShippingRates();
+
+        if (
+            count($_shippingRateGroups) == 1 &&
+            count(current($_shippingRateGroups)) == 1 &&
+            $_shippingAddress->getShippingMethod() &&
+            Mage::getModel('lessfriction/config')->preselectSingleShippingMethod()
+        ) {
+            return false;
+        }
+
+        return true;
+    }
+}
---
+++ app/code/community/KL/LessFriction/Block/Shipping/Method/Available.php
@@ -0,0 +1,5 @@
+<?php
+class KL_LessFriction_Block_Shipping_Method_Available
+    extends Mage_Checkout_Block_Onepage_Shipping_Method_Available
+{
+}
