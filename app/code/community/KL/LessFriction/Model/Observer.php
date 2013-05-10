6f041925cc6be866f5fcf38c92b34c248b3f0220
Andreas Karlsson <andreas.karlsson@indiebytes.se>
2013-05-10 17:00:34 +0200

    Refactor observer

--- app/code/community/KL/LessFriction/Model/Observer.php
+++ app/code/community/KL/LessFriction/Model/Observer.php
@@ -6,10 +6,14 @@ class KL_LessFriction_Model_Observer
 
     var $_defaultCountry = null;
 
+    /**
+     * Pre dispatch
+     *
+     * @return void
+     */
     public function preDispatch(Varien_Event_Observer $observer) {
-        $quote = Mage::getSingleton('checkout/session')->getQuote();
-
-        $_config = Mage::getSingleton('lessfriction/config');
+        /* @var $quote Mage_Sales_Model_Quote */
+        $quote = $this->_getQuote();
 
         /* @var $address Mage_Sales_Model_Quote_Address */
         $billingAddress  = $quote->getBillingAddress();
@@ -21,10 +25,64 @@ class KL_LessFriction_Model_Observer
             if (!$billingAddress->getCountryId()) {
                 $billingAddress->setCountryId($this->_getDefaultCountry()->getCountryId())->save();
             }
+        }
 
-            $store = $quote ? $quote->getStoreId() : null;
+        if ($shippingAddress) {
+            if (!$shippingAddress->getCountryId() || $shippingAddress->getShippingRatesCollection()->count() == 1) {
+                $shippingAddress->setCountryId($this->_getDefaultCountry()->getCountryId())
+                        ->setCollectShippingRates(true)
+                        ->collectShippingRates()
+                        ->save();
+            } else if ($shippingAddress->getCountryId()) {
+                $shippingAddress->setCollectShippingRates(true)->collectShippingRates()->save();
+            }
+        }
+
+        if ($billingAddress && $shippingAddress) {
+            /**
+             * Try setting payment method
+             **/
+            $this->_setPaymentMethod();
+
+            /**
+             * Try setting shipping method
+             **/
+            $this->_setShippingMethod();
+        }
+    }
+
+    /**
+     * Get default country
+     *
+     * @return object
+     */
+    protected function _getDefaultCountry()
+    {
+        if ($this->_defaultCountry === null) {
+            $countryId = Mage::registry('client_country_id') ?
+                            Mage::registry('client_country_id') :
+                            Mage::helper('core')->getDefaultCountry();
+
+            $this->_defaultCountry = new Varien_Object(array('country_id' => $countryId));
+        }
+
+        return $this->_defaultCountry;
+    }
+
+    /**
+     * Try setting payment method based on configuration
+     *
+     * @return void
+     */
+    protected function _setPaymentMethod()
+    {
+        $config = $this->_getConfig();
+
+        if ($config->preselectSinglePaymentMethod() === true) {
+            $quote   = $this->_getQuote();
+            $store   = $quote ? $quote->getStoreId() : null;
             $methods = Mage::helper('payment')->getStoreMethods($store, $quote);
-            $total = $quote->getBaseSubtotal() + $quote->getShippingAddress()->getBaseShippingAmount();
+            $total   = $quote->getBaseSubtotal() + $quote->getShippingAddress()->getBaseShippingAmount();
 
             foreach ($methods as $key => $method) {
                 if ($this->_canUsePaymentMethod($method)
@@ -42,74 +100,19 @@ class KL_LessFriction_Model_Observer
                     ->savePayment(array('method' => current($availablePaymentMethods)));
             }
         }
-
-        if ($shippingAddress) {
-            if (!$shippingAddress->getCountryId() || $shippingAddress->getShippingRatesCollection()->count() == 1) {
-                $shippingAddress->setCountryId($this->_getDefaultCountry()->getCountryId())
-                        ->setCollectShippingRates(true)
-                        ->collectShippingRates()
-                        ->save();
-            } else if ($shippingAddress->getCountryId()) {
-                $shippingAddress->setCollectShippingRates(true)->collectShippingRates()->save();
-            }
-
-            /**
-             * If there's only one shipping method available we might want to preselect
-             * it based on how the module is configured in admin
-             **/
-            if (
-                $_config->preselectSingleShippingMethod() ||
-                $_config->preselectCheapestShippingMethod()
-            ) {
-                $groups = $shippingAddress->getGroupedAllShippingRates();
-
-                if (!empty($groups)) {
-                    foreach ($groups as $code => $groupItems) {
-                        if (!$_config->preselectCheapestShippingMethod()) {
-                            if ($code == self::LESSFRICTION_FREESHIPPING) {
-                                if (count($groupItems) == 1) {
-                                    $shippingMethod = $groupItems[0];
-                                    $shippingAddress->setShippingMethod($shippingMethod->getCode());
-                                }
-                            } else if (count($groups) == 1 && count($groupItems) == 1) {
-                                $shippingMethod = $groupItems[0];
-                                $shippingAddress->setShippingMethod($shippingMethod->getCode());
-                            }
-                        } else {
-                            foreach ($groupItems as $item) {
-                                if (!isset($cheapestShippingMethod) || $cheapestShippingMethod->getPrice() > $item->getPrice()) {
-                                    $cheapestShippingMethod = $item;
-                                }
-                            }
-                        }
-                    }
-
-                    if ($_config->preselectCheapestShippingMethod() && isset($cheapestShippingMethod)) {
-                        $shippingAddress->setShippingMethod($cheapestShippingMethod->getCode());                        
-                    }
-                }
-            }
-        }
-
-
     }
 
-    protected function _getDefaultCountry()
-    {
-        if ($this->_defaultCountry === null) {
-            $countryId = Mage::registry('client_country_id') ?
-                            Mage::registry('client_country_id') :
-                            Mage::helper('core')->getDefaultCountry();
-
-            $this->_defaultCountry = new Varien_Object(array('country_id' => $countryId));
-        }
-
-        return $this->_defaultCountry;
-    }
-
+    /**
+     * Check if customer can use payment method
+     *
+     * @param object
+     *
+     * @return bool
+     */
     protected function _canUsePaymentMethod($method)
     {
-        $quote = Mage::getSingleton('checkout/session')->getQuote();
+        /* @var $quote Mage_Sales_Model_Quote */
+        $quote = $this->_getQuote();
 
         if (!$method->canUseForCountry($quote->getBillingAddress()->getCountry())) {
             return false;
@@ -131,5 +134,75 @@ class KL_LessFriction_Model_Observer
         }
 
         return true;
+    }
+
+    /**
+     * Try setting shipping method based on configuration
+     *
+     * @return void
+     */
+    protected function _setShippingMethod()
+    {
+        $config = $this->_getConfig();
+
+        /**
+         * If there's only one shipping method available we might want to preselect
+         * it based on how the module is configured in admin
+         **/
+        if ($config->preselectSingleShippingMethod()
+                || $config->preselectCheapestShippingMethod()
+                || $config->hideIfFreeShipping()) {
+
+            /* @var $quote Mage_Sales_Model_Quote */
+            $quote  = $this->_getQuote();
+            $groups = $quote->getShippingAddress()->getGroupedAllShippingRates();
+
+            if (!empty($groups)) {
+                foreach ($groups as $code => $groupItems) {
+                    /**
+                     * Preselect cheapest shipping method
+                     **/
+                    if ($config->preselectCheapestShippingMethod()) {
+                        foreach ($groupItems as $item) {
+                            if (!isset($shippingMethod)
+                                || $shippingMethod->getPrice() > $item->getPrice()) {
+
+                                $shippingMethod = $item;
+                            }
+                        }
+                    } else {
+                        if ($code == self::LESSFRICTION_FREESHIPPING) {
+                            if (count($groupItems) == 1) {
+                                $shippingMethod = $groupItems[0];
+                            }
+                        } else if (count($groups) == 1 && count($groupItems) == 1) {
+                            $shippingMethod = $groupItems[0];
+                        }
+                    }
+                }
+
+                $quote->getShippingAddress()->setShippingMethod($shippingMethod->getCode());
+            }
+        }
+    }
+
+    /**
+     * Get quote
+     *
+     * @return Mage_Sales_Model_Quote
+     */
+    protected function _getQuote()
+    {
+        return Mage::getSingleton('checkout/session')->getQuote();
+    }
+
+    /**
+     * Get config model
+     *
+     * @return KL_LessFriction_Model_Config
+     */
+    protected function _getConfig()
+    {
+        return Mage::getModel('lessfriction/config');
     }
 }
