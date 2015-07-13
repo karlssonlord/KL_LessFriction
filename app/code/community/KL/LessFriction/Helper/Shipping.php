718e19b4e7cf06badd2e36f89b992233a36dfae8
David Wickström <davidwickstrom@gmail.com>
2015-07-13 10:06:59 +0200

    Make helper method give correct answer on free shipping state

--- app/code/community/KL/LessFriction/Helper/Shipping.php
+++ app/code/community/KL/LessFriction/Helper/Shipping.php
@@ -5,13 +5,23 @@
  */
 class KL_LessFriction_Helper_Shipping extends Mage_Core_Helper_Abstract
 {
+    /**
+     * There are a bunch of different shipping methods that has a "free shipping" option.
+     * List them here to cover more cases
+     *
+     * @var array
+     */
+    protected $freeShippingMethods = array(
+        'carriers/freeshipping/active',
+        // 'matrixrates/something/something'...
+    );
 
     /**
      * @return bool
      */
     public function customerIsEligibleForFreeShipping()
     {
-        return $this->calculateAmountLeftUntilFreeShippingActivates() == 0;
+        return $this->calculateAmountLeftUntilFreeShippingActivates() == 0 && $this->freeShippingMethodIsEnabled();
     }
 
     /**
@@ -31,6 +41,15 @@ class KL_LessFriction_Helper_Shipping extends Mage_Core_Helper_Abstract
         $totals = Mage::getSingleton('checkout/cart')->getQuote()->getTotals();
 
         return $totals['subtotal']->getValueInclTax();
+    }
+
+    public function freeShippingMethodIsEnabled()
+    {
+        $active = array();
+        foreach ($this->freeShippingMethods as $method) {
+            $active[] = Mage::getStoreConfig($method);
+        }
+        return in_array("1", $active);
     }
 
 }
--- app/code/community/KL/LessFriction/Model/Validator/CustomerEligibleForFreeShipping.php
+++ app/code/community/KL/LessFriction/Model/Validator/CustomerEligibleForFreeShipping.php
@@ -6,17 +6,6 @@
 class KL_LessFriction_Model_Validator_CustomerEligibleForFreeShipping implements KL_LessFriction_Model_Validator
 {
     /**
-     * There are a bunch of different shipping methods that has a "free shipping" option.
-     * List them here to cover more cases
-     *
-     * @var array
-     */
-    protected $freeShippingMethods = array(
-        'carriers/freeshipping/active',
-        // 'matrixrates/something/something'...
-    );
-
-    /**
      * @return bool
      */
     public function validate()
@@ -40,11 +29,7 @@ class KL_LessFriction_Model_Validator_CustomerEligibleForFreeShipping implements
      */
     protected function freeShippingMethodIsEnabled()
     {
-        $active = array();
-        foreach ($this->freeShippingMethods as $method) {
-            $active[] = Mage::getStoreConfig($method);
-        }
-        return in_array("1", $active);
+        return Mage::helper('lessfriction/shipping')->freeShippingMethodIsEnabled();
     }
 
 }
