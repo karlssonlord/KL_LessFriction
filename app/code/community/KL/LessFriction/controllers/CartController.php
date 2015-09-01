0ab3750390a1bb4260624d78e38a4e89272a95fe
Andreas Karlsson <andreas.karlsson@indiebytes.se>
2015-09-01 23:50:35 +0200

    Add totals block to JSON return in updateItemOptionsJsonAction()

--- app/code/community/KL/LessFriction/controllers/CartController.php
+++ app/code/community/KL/LessFriction/controllers/CartController.php
@@ -276,6 +276,7 @@ class KL_LessFriction_CartController extends Mage_Checkout_CartController
 
         $result['blocks'] = $this->_getBlocksAsJson(
             array(
+                'totals',
                 'cart',
                 'shipping_method',
                 'payment',
